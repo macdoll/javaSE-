a688573792129678d2edbd62cecaf0d5ca1f06ea
zxg <2446256163@qq.com>
2021-01-08 22:18:07 +0800

    Merge branch 'master' of https://github.com/macdoll/javase
    # Conflicts:
    #	src/main/java/com/zxg/io/exer.java

---
+++ src/main/java/com/zxg/thread/testMultiThreadClass.java
@@ -0,0 +1,17 @@
+package com.zxg.thread;
+
+public class testMultiThreadClass {
+    public static void main(String[] args) {
+        System.out.println("主线程的方法");
+        multiThreadClass m1 = new multiThreadClass();
+        multiThreadClass m2 = new multiThreadClass();
+        multiThreadClass m3 = new multiThreadClass();
+        multiThreadClass m4 = new multiThreadClass();
+        m1.start();
+        m2.start();
+        m3.start();
+        m4.start();
+
+
+    }
+}
